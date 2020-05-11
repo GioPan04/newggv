8bf915cd8e6b400c3f9c0cbaef3d604fcfd5ca8a
FedericoCarboni <fr3ddydev@gmail.com>
2020-05-11 19:58:06 +0200

    Login page

--- web/src/Login.svelte
+++ web/src/Login.svelte
@@ -1,10 +1,40 @@
 <script>
     let username = '';
     let password = '';
+
+    let loginStatus;
+
+    async function login() {
+        const formData = new FormData();
+        formData.append('username', username);
+        formData.append('password', password);
+
+        const res = await fetch('/api/login', {
+            method: 'POST',
+            body: formData
+        });
+
+        const data = await res.json();
+
+        if (!data.logged) {
+            throw new Error(data.error);
+        }
+    }
 </script>
 
 <main>
-    <input type="text" bind:value={username}/>
-    <input type="password" bind:value={password}/>
-    {username}
+    <form>
+        <input type="text" bind:value={username}/>
+        <input type="password" bind:value={password}/>
+        <input type="submit" value="Login" on:click={() => loginStatus = login()}/>
+    </form>
+    {#if loginStatus}
+        {#await loginStatus}
+            Login...
+        {:then _}
+            Loggato
+        {:catch err}
+            {err.message}
+        {/await}
+    {/if}
 </main>
